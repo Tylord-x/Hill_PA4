64e83273b08b1125497363474d3e695bac4a2366
Tyler <tylerhill420@gmail.com>
2019-11-02 13:28:34 -0400

    update part 2

--- hill_p2/src/hill_p2/Application.java
+++ hill_p2/src/hill_p2/Application.java
@@ -1,10 +1,14 @@
 package hill_p2;
 
+//import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		DuplicateCounter dc=new DuplicateCounter();
+	       dc.count("problem2.txt");
+	       dc.write("unique_word_counts.txt");
 	}
 
 }
--- hill_p2/src/hill_p2/DuplicateCounter.java
+++ hill_p2/src/hill_p2/DuplicateCounter.java
@@ -1,10 +1,49 @@
 package hill_p2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
 public class DuplicateCounter {
+	private int wordCounter;
+	private Map<String, Integer> countWords;
+	   public DuplicateCounter() {
+	       this.countWords = new HashMap<String, Integer>();
+	   }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	   public void count(String dataFile) throws FileNotFoundException {
+	       Scanner scnr = new Scanner(new File(dataFile));
+	      
+	       while (scnr.hasNext()) {
+	    	   String word = scnr.next();
+	    	   Integer count = countWords.get(word);
+	    	   wordCounter++;
+	    	   if (count != null) {
+	    		   count = count + 1;
+	    	   } 
+	    	   else {
+	    		   count = 1;
+	    	   }
+	    	   countWords.put(word, count);
+	       }
+	       //scnr.close();
 
+	   }
+	  
+	   public void write(String outputFile) throws IOException
+	   {
+	       FileWriter fw=new FileWriter(new File(outputFile));
+	       for (Entry<String, Integer> entry : countWords.entrySet()) {
+	    	   fw.write(entry.getKey() + " " + entry.getValue()+"\n");
+	       }
+	       fw.write("Number of words in the file :"+wordCounter+"\n");
+	       fw.close();
+	      
+	   }
 	}
 
-}
